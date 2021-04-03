938fcfdf6eb402e508224628cb3ce7bc53379aeb
applevalley <skatkd7571@naver.com>
2021-04-03 23:55:43 +0900

    0403

---
+++ 02/BinaryTest.java
@@ -0,0 +1,18 @@
+package ch04;
+
+public class BinaryTest {
+
+	public static void main(String[] args) {
+		
+		int num = 10;
+		int binaryNum = 0B1010;
+		int octaNum = 012;
+		int xnum = 0XA;
+		
+		System.out.println(num);
+		System.out.println(binaryNum);
+		System.out.println(octaNum);
+		System.out.println(xnum);
+	}
+
+}
---
+++ 02/VariableTest.java
@@ -0,0 +1,16 @@
+package ch06;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		
+		byte bnum = 127;
+		
+		System.out.println(bnum);
+		
+		long lnum = 8397223432L;
+		
+		System.out.println(lnum);
+	}
+
+}
